c7e09340f6a1632e2b5c48d9911f52b3f99d8df2
Tom Seldon <tom@tomseldon.co.uk>
2014-08-10 12:07:11 +0100

    jshint reporter: Skip successful files

---
+++ tests/jshint-adapter.spec.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var chai = require('chai'),
+    sinon = require('sinon'),
+    sinonChai = require('sinon-chai'),
+    mocha = require('mocha'),
+    File = require('vinyl'),
+    through = require('through2'),
+    JshintAdapter = require('../lib/util/jshint-adapter');
+
+// Setup test tools
+chai.should();
+chai.use(sinonChai);
+
+describe('Jshint Reporter Adapter', function() {
+    var jshintAdapter;
+
+    afterEach(function() {
+        jshintAdapter = null;
+    });
+
+    it(
+        'should convert a successful gjslint result object ' +
+            'into a jshint compatible one',
+        function() {
+            var file, parsedFile;
+
+            file = new File();
+
+            file.gjslint = {
+                success: true
+            };
+
+            parsedFile = new JshintAdapter(file);
+
+            parsedFile.should.have.property('jshint');
+            parsedFile.jshint.should.have.property('success', true);
+        }
+    );
+
+    it(
+        'should convert a failed gjslint result object ' +
+            'into a jshint compatible one',
+        function() {
+            var file, parsedFile, mockError;
+
+            file = new File({path: './fake.js'});
+
+            mockError = {
+                code: 2,
+                line: 10,
+                description: 'foo'
+            };
+
+            file.gjslint = {
+                success: false,
+                errors: [mockError]
+            };
+
+            parsedFile = new JshintAdapter(file);
+
+            parsedFile.should.have.property('jshint');
+            parsedFile.jshint.should.have.property('success', false);
+            parsedFile.jshint.should.have.property('results');
+            parsedFile.jshint.results.length.should.equal(1);
+
+            parsedFile.jshint.results[0].error.reason
+                .should.equal(mockError.description);
+
+            parsedFile.jshint.results[0].error.code
+                .should.equal('Warning');
+
+            parsedFile.jshint.results[0].error.line
+                .should.equal(mockError.line);
+        }
+    );
+});
